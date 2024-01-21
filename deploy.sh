5652d2651cee31253e625c5f4fdb1954a96c9058
Cristina Conde Serrano <117383375+CrisConSer@users.noreply.github.com>
2024-01-21 14:09:07 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Establecer las credenciales
+export AWS_ACCESS_KEY_ID=ASIAXKD7UYOQHDE7IQ66
+export AWS_SECRET_ACCESS_KEY=CuSfNGAPxbbX9cOcAMAj5Bng3//SIF/kLB8yHzxQ
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzEIX//////////wEaDOcH7Yhyr70NmJrN9yK/AevxMH3sNQE5CNXsSUxzzZQXnsTHmRbbVnC1BHJcHwHeZPykaV3TmYtJT0053JMM7hXacWCywthAO81cbn8A4jUFgSg9Nzf3gHaPIbWqZ++/4KQuYxaiR7BdgV9KMHG5sFxeH>
+
 # Establecer la región deseada
 export AWS_DEFAULT_REGION=us-east-1d
 
